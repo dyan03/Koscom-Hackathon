8f7bd98b87774b12fc1a207ad1a48b28543181e9
DESKTOP-AT1HIJB\ahn <theta93@gmail.com>
2020-01-28 23:23:16 +0900

    scroll추가

--- src/pages/FundInfo/index.js
+++ src/pages/FundInfo/index.js
@@ -1,60 +1,76 @@
 import React, { useState } from 'react';
 
 function FundInfo() {
-    const [name, setName]=useState('아무개');
-    const [userType, setUserType]=useState('투자자');
+  const [name, setName] = useState('아무개');
+  const [userType, setUserType] = useState('투자자');
 
-    //로그인된 유저 id로 DB에서 보유펀드정보 불러옴
-    const fundWait = {
+  //로그인된 유저 id로 DB에서 보유펀드정보 불러옴
+  const fundWait = {
 
-    }
-    const fundIng = {}
-    const fundEnd = {}
-    const fundCanceled = {}
+  }
+  const fundIng = {}
+  const fundEnd = {}
+  const fundCanceled = {}
 
-    return (
+  return (
 
-        <div>
-            <div >
-            <h4 style={{textAlign: 'center', marginTop: 20}}>
-            {userType} {name}님의 펀드 목록입니다.
+    <div>
+      <div >
+        <h4 style={{ textAlign: 'center', marginTop: 20 }}>
+          {userType} {name}님의 펀드 목록입니다.
             </h4>
-            </div>
-            
-            <div style={{margin: 'auto', marginTop: 30, width: 700}}>
-            <div class="list-group" >
-                <button type="button" class="list-group-item list-group-item-action active">
-                    대기중인 펀드
-                </button>
-                <button type="button" class="list-group-item list-group-item-action">펀드 1호 <span class="badge badge-primary badge-pill" style={{textAlign:'right'}}>14</span></button>
-                <button type="button" class="list-group-item list-group-item-action">펀드 2호</button>
+      </div>
 
-            </div>
-            <div class="list-group">
-                <button type="button" class="list-group-item list-group-item-action active">
-                    운용중인 펀드
-                </button>
-                <button type="button" class="list-group-item list-group-item-action">펀드 1호</button>
-                <button type="button" class="list-group-item list-group-item-action">펀드 2호</button>
+      <div style={{ margin: 'auto', marginTop: 30, width: 700 }}>
+        <div class="list-group" >
+          <button type="button" class="list-group-item list-group-item-action active">
+            대기중인 펀드<span class="badge badge-primary badge-pill" style={{ textAlign: 'right', backgroundColor:'white', color: 'black' }}>6</span>
+          </button>
+          <div style={{ overflowY: 'scroll', height: 200 }}>
+            <button type="button" class="list-group-item list-group-item-action">펀드 1호 </button>
+            <button type="button" class="list-group-item list-group-item-action">펀드 2호</button>
+            <button type="button" class="list-group-item list-group-item-action">펀드 3호</button>
+            <button type="button" class="list-group-item list-group-item-action">펀드 4호</button>
+            <button type="button" class="list-group-item list-group-item-action">펀드 5호</button>
+            <button type="button" class="list-group-item list-group-item-action">펀드 6호</button>
 
-            </div>
-            <div class="list-group">
-                <button type="button" class="list-group-item list-group-item-action active">
-                    운용마감된 펀드
-                </button>
-                <button type="button" class="list-group-item list-group-item-action">펀드 1호</button>
-            </div>
-            <div class="list-group">
-                <button type="button" class="list-group-item list-group-item-action active">
-                    취소된 펀드
-                </button>
-                <button type="button" class="list-group-item list-group-item-action">펀드 1호</button>
-                <button type="button" class="list-group-item list-group-item-action">펀드 2호</button>
+          </div>
 
-            </div>
         </div>
+        <div class="list-group">
+          <button type="button" class="list-group-item list-group-item-action active">
+            운용중인 펀드<span class="badge badge-primary badge-pill" style={{ textAlign: 'right', backgroundColor:'white', color: 'black' }}>6</span>
+                </button>
+          <div style={{ overflowY: 'scroll', height: 200 }}>
+            <button type="button" class="list-group-item list-group-item-action">펀드 1호</button>
+            <button type="button" class="list-group-item list-group-item-action">펀드 2호</button>
+            <button type="button" class="list-group-item list-group-item-action">펀드 3호</button>
+            <button type="button" class="list-group-item list-group-item-action">펀드 4호</button>
+            <button type="button" class="list-group-item list-group-item-action">펀드 4호</button>
+            <button type="button" class="list-group-item list-group-item-action">펀드 4호</button>
+          </div>
+
         </div>
-    )
+        <div class="list-group">
+          <button type="button" class="list-group-item list-group-item-action active">
+            운용마감된 펀드<span class="badge badge-primary badge-pill" style={{ textAlign: 'right', backgroundColor:'white', color: 'black' }}>1</span>
+          </button>
+          <button type="button" class="list-group-item list-group-item-action">펀드 1호</button>
+        </div>
+        <div class="list-group">
+          <button type="button" class="list-group-item list-group-item-action active">
+            취소된 펀드<span class="badge badge-primary badge-pill" style={{ textAlign: 'right', backgroundColor:'white', color: 'black' }}>6</span>
+          </button>
+          <div style={{ overflowY: 'scroll', height: 200 }}>
+            <button type="button" class="list-group-item list-group-item-action">펀드 1호</button>
+            <button type="button" class="list-group-item list-group-item-action">펀드 2호</button>
+            <button type="button" class="list-group-item list-group-item-action">펀드 3호</button>
+            <button type="button" class="list-group-item list-group-item-action">펀드 4호</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 
