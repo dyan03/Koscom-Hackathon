55c8bde2e2b85e99e885c7b9fd7de8594b826193
DESKTOP-AT1HIJB\ahn <theta93@gmail.com>
2020-01-28 23:04:00 +0900

    fundinfo2

--- src/pages/FundInfo/index.js
+++ src/pages/FundInfo/index.js
@@ -26,7 +26,7 @@ function FundInfo() {
                 <button type="button" class="list-group-item list-group-item-action active">
                     대기중인 펀드
                 </button>
-                <button type="button" class="list-group-item list-group-item-action">펀드 1호</button>
+                <button type="button" class="list-group-item list-group-item-action">펀드 1호 <span class="badge badge-primary badge-pill" style={{textAlign:'right'}}>14</span></button>
                 <button type="button" class="list-group-item list-group-item-action">펀드 2호</button>
 
             </div>
@@ -56,5 +56,36 @@ function FundInfo() {
         </div>
     )
 }
+
+
+
+{/* <div class="list-group">
+  <a href="#" class="list-group-item list-group-item-action flex-column align-items-start active">
+    <div class="d-flex w-100 justify-content-between">
+      <h5 class="mb-1">List group item heading</h5>
+      <small>3 days ago</small>
+    </div>
+    <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+    <small>Donec id elit non mi porta.</small>
+  </a>
+  <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+    <div class="d-flex w-100 justify-content-between">
+      <h5 class="mb-1">List group item heading</h5>
+      <small class="text-muted">3 days ago</small>
+    </div>
+    <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+    <small class="text-muted">Donec id elit non mi porta.</small>
+  </a>
+  <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+    <div class="d-flex w-100 justify-content-between">
+      <h5 class="mb-1">List group item heading</h5>
+      <small class="text-muted">3 days ago</small>
+    </div>
+    <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+    <small class="text-muted">Donec id elit non mi porta.</small>
+  </a>
+</div> */}
+
+
 
 export default FundInfo;
