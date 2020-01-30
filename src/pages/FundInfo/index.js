b7c18253b121fd240db8fa162426774b6d523ac2
dyan03 <theta93@gmail.com>
2020-01-30 09:20:09 +0900

    내 펀드 리스트 DB 불러오기 성공 -settimeout으로 구현

--- src/pages/FundInfo/index.js
+++ src/pages/FundInfo/index.js
@@ -4,8 +4,8 @@ import Popup from './popup'
 function FundInfo() {
   const [name, setName] = useState('아무개');
   const [userType, setUserType] = useState('투자자');
-  const [fund, setFund]=useState();
-  const [activate,setActivate]=useState(true);
+  const [fund, setFund] = useState();
+  const [activate, setActivate] = useState(true);
 
   //로그인된 유저 id로 DB에서 보유펀드정보 불러옴
   const fundWait = {
@@ -28,7 +28,7 @@ function FundInfo() {
     )
   }
 
-  function foo(){
+  function foo() {
     fetch("http://localhost:8551/myFund", {
       method: 'POST',
       body: JSON.stringify({
@@ -36,25 +36,42 @@ function FundInfo() {
         // 'fundStage': stage,
       }),
       headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
-      })
+    })
       .then((res) => (console.dir(res.json().then(data => {
-        console.log("현재 타입",data.fundList); 
-        result=data.fundList
+        console.log("현재 타입", data.fundList);
+        result = data.fundList
         setFund(result)
-        setActivate(!activate)
+        setActivate(false)
       }))))
   }
 
-  return (
+  if (activate) {
+    var intervalID = setInterval(foo, 300);
+    setTimeout(function () {
+      clearInterval(intervalID);
+    }, 301);
+  }
 
+
+  return (
     <div>
       <div >
         <h4 style={{ textAlign: 'center', marginTop: 20 }}>
-          {userType} {name}님의 펀드 목록입니다. 
+
+          {activate
+            ?
+            <div>
+              정보를 불러오는 중입니다.
+            </div>
+            :
+            <div>
+              {userType} {name}님의 펀드 목록입니다.
+            </div>
+          }
         </h4>
-        <button type="button" class="list-group-item list-group-item-action active" onClick={foo}>
-            정보불러오기<span class="badge badge-primary badge-pill" style={{ textAlign: 'right', backgroundColor: 'white', color: 'black' }}>6</span>
-        </button>
+        {/* <button type="button" class="list-group-item list-group-item-action active" onClick={foo}>
+          정보불러오기<span class="badge badge-primary badge-pill" style={{ textAlign: 'right', backgroundColor: 'white', color: 'black' }}>6</span>
+        </button> */}
       </div>
       <div style={{ margin: 'auto', marginTop: 30, width: 700 }}>
         <div class="list-group" >
@@ -64,35 +81,35 @@ function FundInfo() {
           <div style={{ overflowY: 'scroll', height: 200 }}>
 
             {activate
-            ?            
-            <div>
-              정보를 불러와주세요
+              ?
+              <div>
+                정보를 불러오는 중입니다.
             </div>
-            :
-            <div>
-            {fund.fund_id}
-            {fund.map(obj=>{
-              return (
-                <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
-                <div class="d-flex w-100 justify-content-between">
-                  <h5 class="mb-1">{obj.fund_id} </h5>
-                  <small class="text-muted">3 days ago</small>
-                </div>
-                <p class="mb-1">{obj.register_email} {obj.stage} {obj.total_amount} {obj.current_amount}</p>
-              </a>
-                )
-            })}
-            </div>
-            
-            }
-            <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
-              <div class="d-flex w-100 justify-content-between">
-                <h5 class="mb-1">List group item heading</h5>
-                <small class="text-muted">3 days ago</small>
+              :
+              <div>
+                {fund.fund_id}
+                {fund.map(obj => {
+                  return (
+                    <div>
+                      {obj.stage === 0
+                        ?
+                        <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+                          <div class="d-flex w-100 justify-content-between">
+                            <h5 class="mb-1">{obj.fund_id} </h5>
+                            <small class="text-muted">3 days ago</small>
+                          </div>
+                          <p class="mb-1">{obj.register_email} {obj.stage} {obj.total_amount} {obj.current_amount}</p>
+                        </a>
+                        :
+                        <div>
+                        </div>
+                      }
+                    </div>
+                  )
+                })}
               </div>
-              <p class="mb-1">Donec id elit non mi porta gravida at eget metus.</p>
-              <small class="text-muted">Donec id elit non mi porta.</small>
-            </a>
+            }
+
           </div>
 
         </div>
@@ -101,12 +118,36 @@ function FundInfo() {
             운용중인 펀드<span class="badge badge-primary badge-pill" style={{ textAlign: 'right', backgroundColor: 'white', color: 'black' }}>6</span>
           </button>
           <div style={{ overflowY: 'scroll', height: 200 }}>
-            <button type="button" class="list-group-item list-group-item-action">펀드 1호</button>
-            <button type="button" class="list-group-item list-group-item-action">펀드 2호</button>
-            <button type="button" class="list-group-item list-group-item-action">펀드 3호</button>
-            <button type="button" class="list-group-item list-group-item-action">펀드 4호</button>
-            <button type="button" class="list-group-item list-group-item-action">펀드 4호</button>
-            <button type="button" class="list-group-item list-group-item-action">펀드 4호</button>
+
+          {activate
+              ?
+              <div>
+                정보를 불러오는 중입니다.
+            </div>
+              :
+              <div>
+                {fund.fund_id}
+                {fund.map(obj => {
+                  return (
+                    <div>
+                      {obj.stage === 1
+                        ?
+                        <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+                          <div class="d-flex w-100 justify-content-between">
+                            <h5 class="mb-1">{obj.fund_id} </h5>
+                            <small class="text-muted">3 days ago</small>
+                          </div>
+                          <p class="mb-1">{obj.register_email} {obj.stage} {obj.total_amount} {obj.current_amount}</p>
+                        </a>
+                        :
+                        <div>
+                        </div>
+                      }
+                    </div>
+                  )
+                })}
+              </div>
+            }
           </div>
 
         </div>
@@ -114,17 +155,72 @@ function FundInfo() {
           <button type="button" class="list-group-item list-group-item-action active">
             운용마감된 펀드<span class="badge badge-primary badge-pill" style={{ textAlign: 'right', backgroundColor: 'white', color: 'black' }}>1</span>
           </button>
-          <button type="button" class="list-group-item list-group-item-action">펀드 1호</button>
+          
+          {activate
+              ?
+              <div>
+                정보를 불러오는 중입니다.
+            </div>
+              :
+              <div>
+                {fund.fund_id}
+                {fund.map(obj => {
+                  return (
+                    <div>
+                      {obj.stage === 2
+                        ?
+                        <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+                          <div class="d-flex w-100 justify-content-between">
+                            <h5 class="mb-1">{obj.fund_id} </h5>
+                            <small class="text-muted">3 days ago</small>
+                          </div>
+                          <p class="mb-1">{obj.register_email} {obj.stage} {obj.total_amount} {obj.current_amount}</p>
+                        </a>
+                        :
+                        <div>
+                        </div>
+                      }
+                    </div>
+                  )
+                })}
+              </div>
+            }
         </div>
         <div class="list-group">
           <button type="button" class="list-group-item list-group-item-action active">
             취소된 펀드<span class="badge badge-primary badge-pill" style={{ textAlign: 'right', backgroundColor: 'white', color: 'black' }}>6</span>
           </button>
           <div style={{ overflowY: 'scroll', height: 200 }}>
-            <button type="button" class="list-group-item list-group-item-action">펀드 1호</button>
-            <button type="button" class="list-group-item list-group-item-action">펀드 2호</button>
-            <button type="button" class="list-group-item list-group-item-action">펀드 3호</button>
-            <button type="button" class="list-group-item list-group-item-action">펀드 4호</button>
+
+          {activate
+              ?
+              <div>
+                정보를 불러오는 중입니다.
+            </div>
+              :
+              <div>
+                {fund.fund_id}
+                {fund.map(obj => {
+                  return (
+                    <div>
+                      {obj.stage === 3
+                        ?
+                        <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+                          <div class="d-flex w-100 justify-content-between">
+                            <h5 class="mb-1">{obj.fund_id} </h5>
+                            <small class="text-muted">3 days ago</small>
+                          </div>
+                          <p class="mb-1">{obj.register_email} {obj.stage} {obj.total_amount} {obj.current_amount}</p>
+                        </a>
+                        :
+                        <div>
+                        </div>
+                      }
+                    </div>
+                  )
+                })}
+              </div>
+            }
           </div>
         </div>
       </div>
